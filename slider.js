46b68ef1fdd27230256d7e8fd5ca9106e7e2103f
Mthobisi-za <mthobisimtho333@gmail.com>
2023-07-24 13:46:24 +0200

    done

--- slider.js
+++ slider.js
@@ -1,3 +1,9 @@
+var collection = [
+    { name: 'amagwinya', url: '/assets/titles/Group 109.png' },
+    { name: 'amagwinya', url: '/assets/titles/Group 112.png' },
+    { name: 'boots', url: '/assets/titles/Group 107.png' },
+
+]
 document.addEventListener('DOMContentLoaded', function() {
     var splide = new Splide('.splide', {
         type: 'loop',
@@ -19,4 +25,22 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
     splide.mount();
+    splide.on('move', (e) => {
+        var index = Number(e);
+        console.log(index)
+        if (e == 1) {
+            document.querySelector('.first').src = collection[1].url;
+            document.querySelector('.second').src = collection[2].url;
+            document.querySelector('.third').src = collection[0].url;
+        } else if (e == 2) {
+            document.querySelector('.first').src = collection[2].url;
+            document.querySelector('.second').src = collection[0].url;
+            document.querySelector('.third').src = collection[1].url;
+        } else if (e == 0) {
+            document.querySelector('.first').src = collection[0].url;
+            document.querySelector('.second').src = collection[1].url;
+            document.querySelector('.third').src = collection[2].url;
+        }
+
+    });
 });
